8de2db71cd6e7236e6948b5e177d37330e23b143
khaleel-cloud786 <58719606+khaleel-cloud786@users.noreply.github.com>
2021-01-12 13:01:57 +0530

    Add files via upload

---
+++ nodejs/index.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const app = express();
+
+const connection=mysql.createConnection({
+    host:'nodejs-mysql.default.svc.cluster.local',
+    user:'root',
+    password:'khaleel',
+    database:'node_crud'
+});
+
+connection.connect(function(error){
+    if(!!error) console.log(error);
+    else console.log('Database Connected!');
+});
+
+app.set('views',path.join(__dirname,'views'));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.get('/',(req, res) => {
+    let sql = "SELECT * FROM users";
+    let query = connection.query(sql, (err, rows) => {
+        if(err) throw err;
+        res.render('user_index', {
+            title : 'CRUD Operation using NodeJS / ExpressJS / MySQL / Kubernetes',
+            users : rows
+        });
+    });
+});
+
+
+app.get('/add',(req, res) => {
+    res.render('user_add', {
+        title : 'CRUD Operation using NodeJS / ExpressJS / MySQL / Kubernetes'
+    });
+});
+
+app.post('/save',(req, res) => {
+    let data = {name: req.body.name, email: req.body.email, phone_no: req.body.phone_no};
+    let sql = "INSERT INTO users SET ?";
+    let query = connection.query(sql, data,(err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+});
+
+app.get('/edit/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `Select * from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.render('user_edit', {
+            title : 'CRUD Operation using NodeJS / ExpressJS / MySQL / Kubernetes',
+            user : result[0]
+        });
+    });
+});
+
+
+app.post('/update',(req, res) => {
+    const userId = req.body.id;
+    let sql = "update users SET name='"+req.body.name+"',  email='"+req.body.email+"',  phone_no='"+req.body.phone_no+"' where id ="+userId;
+    let query = connection.query(sql,(err, results) => {
+      if(err) throw err;
+      res.redirect('/');
+    });
+});
+
+
+app.get('/delete/:userId',(req, res) => {
+    const userId = req.params.userId;
+    let sql = `DELETE from users where id = ${userId}`;
+    let query = connection.query(sql,(err, result) => {
+        if(err) throw err;
+        res.redirect('/');
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running at port 3000');
+});
+
